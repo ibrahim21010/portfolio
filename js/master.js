835f5be063bf0122eb60733b764d50fe549d8464
ibrahim21010 <i.mohsen21010@gmail.com>
2021-09-24 12:06:06 +0200

    fixing some problems in the Css

--- js/master.js
+++ js/master.js
@@ -1,9 +1,15 @@
+/* -------------------------------------------------------------------------- */
+/*                                  init var                                  */
+/* -------------------------------------------------------------------------- */
 const menuIcon = document.querySelector("#menu-bar");
 const navLinks = document.querySelector(".links");
 const closeMenu = document.querySelector("#closeMenu");
+const top_nav = document.querySelector(".top_nav");
 
 
-
+/**--------------------------------------------
+ **     show and hide menu on toggle
+ *---------------------------------------------**/
 menuIcon.addEventListener("click", () => {
 
     if (!navLinks.classList.contains("show")) {
@@ -11,10 +17,24 @@ menuIcon.addEventListener("click", () => {
     } else {
         navLinks.classList.toggle("show");
     }
-
-
-
 });
 closeMenu.addEventListener('click',()=>{
     navLinks.classList.remove("show")
 })
+
+/**--------------------------------------------
+ **     set Header fixed at top when Scrolling
+ *---------------------------------------------**/
+
+ window.onscroll = ()=>{
+     let top = window.scrollY 
+     console.log(top)
+
+     if(top >= 100){
+         console.log("set the fixed bar")
+
+         top_nav.classList.add("fixed")
+     }else{
+        top_nav.classList.remove("fixed")
+     }
+ }
